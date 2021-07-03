ef622a27900d9ba25189117c192b9c94ff9d57ca
Divyanshu545 <75415675+Divyanshu545@users.noreply.github.com>
2021-07-03 17:59:13 +0530

    Add files via upload

---
+++ Devsnet THAs/Day14/func.js
@@ -0,0 +1,19 @@
+function onld() {
+    console.log("Loaded");
+  }
+  
+function onerr() {
+    console.log("Error");
+  }
+
+function mouseDown() {
+    console.log("Mouse Down");
+  }
+
+function oninvld() {
+    console.log("Invalid")
+  }
+
+function onply {
+    console.log("I am Playing")
+  }
