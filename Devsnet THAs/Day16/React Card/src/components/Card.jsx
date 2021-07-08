7819a254357fb77b4a61381c07c458697aefc097
Divyanshu545 <75415675+Divyanshu545@users.noreply.github.com>
2021-07-08 23:48:24 +0530

    Add files via upload

---
+++ Devsnet THAs/Day16/React Card/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card =() =>{
+    return(
+        <div className="card">
+            <img src="/images/bat.jpg" alt="" />
+            <h2>React Card</h2>
+            <p>Batman</p>
+        </div>
+    );
+};
+
+export default Card;
