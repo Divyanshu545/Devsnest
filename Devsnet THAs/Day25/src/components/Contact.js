c81ba96dee7bbebbe17441097762365a32998138
Divyanshu545 <75415675+Divyanshu545@users.noreply.github.com>
2021-08-03 22:32:52 +0530

    Add files via upload

---
+++ Devsnet THAs/Day25/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1 style={{fontSize: '30px',padding: "20px"}}>Python is best</h1>
+            </div>
+        )
+    }
+}
